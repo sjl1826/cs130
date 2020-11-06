47bb2bbb2ec70da75c20d7282a8623884fcc857a
Alexander Trujillo-Hilleary <alexander@Alexanders-MacBook-Pro.local>
2020-11-06 14:44:33 -0800

    Created Landing page

--- cs130_front/src/components/views/LandingPage/LandingPage.js
+++ cs130_front/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from '../../../logo.svg';
+import { css } from 'emotion';
+import { Route } from 'react-router-dom';
+import Auth from '../../../hoc/auth';
+import RegisterPage from '../RegisterPage/RegisterPage.js';
+import studyPic from "../../../studyPic.jpg";
+import Button from "../../Button/Button";
 import '../../../App.css';
+import Text from "../../Text/Text"
 
 function LandingPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={studyPic} className="App-logo" alt="logo" />
+        <p className={css`
+          width: 1680px;
+          height: 100px;
+          font-family: Poppins;`
+        }>
+          Broaden your scope, broaden your network, broaden your mind.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button height="80px" width="200px">
+          <Text size="30px" color="white"> Join </Text>
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+        </Button>
       </header>
     </div>
   );
