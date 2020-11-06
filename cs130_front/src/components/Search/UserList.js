5244eaec684f1fd75755c4ec93ab88b4cbf448e9
Kathir Ilango <32283019+Kathirilango@users.noreply.github.com>
2020-11-06 13:36:05 -0800

    Dynamic search bar front-end (#36)
    * Initial commit for basic dynamic search bar functionality
    
    * functionality to navigate to user profile on click
    
    * search bar styling
    
    Co-authored-by: Kathir Ilango <kathirilango@MacBook-Pro-5.local>

---
+++ cs130_front/src/components/Search/UserList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './UserList.css'
+
+const UserList = (props) => {
+  const ListStyling = {display: "flex", justifyContent: "center"}
+
+  return (
+    <>
+    { props.userList.map((data,index) => {
+        if (data) {
+          return (
+            <div style={ListStyling} key={data.name} onClick={props.goToUserProfile(data)}>
+              <h1 class="listItem" onmouseover="">{data.name}</h1>
+	          </div>	
+    	    );	
+    	  }
+    	  return null;
+    })
+    }
+    </>
+  );
+}
+
+export default UserList
---
+++ cs130_front/src/components/views/SearchPage/SearchPage.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from '../../Search/SearchBar';
+import UserList from '../../Search/UserList';
+import * as Fonts from '../../../constants/Fonts';
+
+const SearchPage = (props) => {
+  const [input, setInput] = useState('');
+  const [userListDefault, setUserListDefault] = useState();
+  const [userList, setUserList] = useState();
+
+  const fetchData = async () => {
+    return await fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+         setUserList(data) 
+         setUserListDefault(data)
+        }
+      );
+  }
+
+  const updateInput = async (input) => {
+    const filtered = userListDefault.filter(user => {
+      return user.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setInput(input);
+    setUserList(filtered);
+  }
+
+  useEffect( () => {fetchData()},[]);
+  const goToUserProfile = user => () => { props.history.push(`/profile/${user.name}`); }
+  
+  return (
+    <div style={{fontFamily: Fonts.Primary, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1 style = {{fontSize: '40px'}}>Search User</h1>
+      <div> 
+        <SearchBar input={input} onChange={updateInput} />
+      </div>
+      { input && <UserList userList={userList} goToUserProfile={goToUserProfile}/> }
+    </div>
+  );
+}
+
+export default SearchPage
