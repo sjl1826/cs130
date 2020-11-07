09037c7fe0364a53a7eb1ef2522346308a9602db
Alexander Trujillo-Hilleary <alexander@Alexanders-MacBook-Pro.local>
2020-11-06 22:13:40 -0800

    Changed the way the css was implemented for Landing Page.

--- cs130_front/src/components/views/LandingPage/LandingPage.js
+++ cs130_front/src/components/views/LandingPage/LandingPage.js
@@ -5,24 +5,27 @@ import Auth from '../../../hoc/auth';
 import RegisterPage from '../RegisterPage/RegisterPage.js';
 import studyPic from "../../../studyPic.jpg";
 import Button from "../../Button/Button";
-import '../../../App.css';
+import './App.css';
 import Text from "../../Text/Text"
 
-function LandingPage() {
+
+function LandingPage(props) {
+  function join() {
+    props.history.push('/register');
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={studyPic} className="App-logo" alt="logo" />
         <p className={css`
-          width: 1680px;
+          width: 100vw;
           height: 100px;
           font-family: Poppins;`
         }>
           Broaden your scope, broaden your network, broaden your mind.
         </p>
-        <Button height="80px" width="200px">
+        <Button height="80px" width="200px" onClick={join}>
           <Text size="30px" color="white"> Join </Text>
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
         </Button>
       </header>
     </div>
