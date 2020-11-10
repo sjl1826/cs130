8f60c4a8aa379bba8ee61b87a14b691dbb64f93c
shirlyfang <shirly.is.here@gmail.com>
2020-11-09 21:18:17 -0800

    Invitation/Requests common component (#41)

--- cs130_front/src/App.js
+++ cs130_front/src/App.js
@@ -24,9 +24,9 @@ function App() {
 					<Route exact path="/" component={Auth(LandingPage, null)} />
 					<Route exact path="/login" component={Auth(LoginPage, false)} />
 					<Route exact path="/register" component={Auth(RegisterPage, false)} />
-          			<Route exact path="/classes/" component={Auth(ClassesPage, true)} />
-					<Route exact path="/groups/" component={Auth(GroupsPage, true)} />
-          			<Route exact path="/profile/" component={Auth(ProfilePage, true)} />
+          <Route exact path="/classes/:id" component={Auth(ClassesPage, true)} />
+					<Route exact path="/groups/:id" component={Auth(GroupsPage, true)} />
+          <Route exact path="/profile/:id" component={Auth(ProfilePage, true)} />
 					<Route exact path="/search/" component={Auth(SearchPage, true)} />
 				</Switch>
 			</div>
