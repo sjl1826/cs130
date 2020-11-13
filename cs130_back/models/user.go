9caa456a0d9ec95d0b1bfeb84fbc75a81dbbc91f
= <heysamlee@gmail.com>
2020-11-12 21:10:17 -0800

    added group, listing, invitation add, remove, get database interaction for user and renamed request to invitation

--- cs130_back/models/user.go
+++ cs130_back/models/user.go
@@ -37,6 +37,52 @@ type User struct {
 	Invitations		pq.Int64Array	`gorm:"type:integer[]" json:"invitations"`
 }
 
+// CreateUser creates the user specified
+func (u *User) CreateUser(db *gorm.DB) error {
+	now := time.Now()
+	u.CreatedAt = now
+	retVal := db.Create(u).Table("users").Scan(&u)
+	return retVal.Error
+}
+
+// GetUser gets specified user and returns it
+func (u *User) GetUser(db *gorm.DB) error {
+	retVal := db.Raw("SELECT * FROM users WHERE ID=" + strconv.Itoa(u.ID)).Scan(&u)
+	return retVal.Error
+}
+
+// UpdateUser updates user fields
+func (u *User) UpdateUser(db *gorm.DB) error {
+	now := time.Now()
+	u.UpdatedAt = now
+	retVal := db.Save(&u).Table("users")
+	return retVal.Error
+}
+
+// DeleteUser deletes the specified user
+func (u *User) DeleteUser(db *gorm.DB) error {
+	retVal := db.Exec("DELETE FROM users WHERE ID=" + strconv.Itoa(u.ID))
+	return retVal.Error
+}
+
+// GetByEmail gets the user by email, not ID
+func (u *User) GetByEmail(db *gorm.DB) error {
+	row := db.Raw("SELECT id FROM users WHERE email='" + u.Email + "'").Row()
+	var i int
+	row.Scan(&i)
+	u.ID = i
+	error := u.GetUser(db)
+	return error
+}
+
+// GetPassword gets the hashed password for the user
+func (u *User) GetPassword(db *gorm.DB) {
+	row := db.Raw("SELECT password FROM users where email='" + u.Email + "'").Row()
+	var pass string
+	row.Scan(&pass)
+	u.Password = pass
+}
+
 // AddGroup adds a new group to the user
 func (u *User) AddGroup(db *gorm.DB, groupID int) error {
 	now := time.Now()
@@ -105,50 +151,72 @@ func (u *User) GetCourses(db *gorm.DB, courseList *[]Course) error {
 	return retVal.Error
 }
 
-// CreateUser creates the user specified
-func (u *User) CreateUser(db *gorm.DB) error {
-	now := time.Now()
-	u.CreatedAt = now
-	retVal := db.Create(u).Table("users").Scan(&u)
-	return retVal.Error
-}
-
-// GetUser gets specified user and returns it
-func (u *User) GetUser(db *gorm.DB) error {
-	retVal := db.Raw("SELECT * FROM users WHERE ID=" + strconv.Itoa(u.ID)).Scan(&u)
-	return retVal.Error
-}
-
-// UpdateUser updates user fields
-func (u *User) UpdateUser(db *gorm.DB) error {
+// AddListing adds a new listing to the user
+func (u *User) AddListing(db *gorm.DB, listingID int) error {
 	now := time.Now()
 	u.UpdatedAt = now
+	u.Listings = append(u.Listings, int64(listingID))
 	retVal := db.Save(&u).Table("users")
 	return retVal.Error
 }
 
-// DeleteUser deletes the specified user
-func (u *User) DeleteUser(db *gorm.DB) error {
-	retVal := db.Exec("DELETE FROM users WHERE ID=" + strconv.Itoa(u.ID))
+// RemoveListing removes the specified Listing from the user
+func (u *User) RemoveListing(db *gorm.DB, listingID int) error {
+	now := time.Now()
+	u.UpdatedAt = now
+	for i, g := range u.Listings {
+		if g == int64(listingID) {
+			u.Listings = RemoveElement(u.Listings, i)
+			break
+		}
+	}
+	retVal := db.Save(&u).Table("users")
 	return retVal.Error
 }
 
-// GetByEmail gets the user by email, not ID
-func (u *User) GetByEmail(db *gorm.DB) error {
-	row := db.Raw("SELECT id FROM users WHERE email='" + u.Email + "'").Row()
-	var i int
-	row.Scan(&i)
-	u.ID = i
-	error := u.GetUser(db)
-	return error
+// GetListings retrieves the listing objects under the user
+func (u *User) GetListings(db *gorm.DB, listingList *[]Listing) error {
+	retVal := db.Raw("SELECT * FROM users WHERE ID=" + strconv.Itoa(u.ID)).Scan(&u)
+	for _, g := range u.Listings {
+		tempListing := Listing{ID: int(g)}
+		db.Raw("SELECT * FROM listings WHERE ID=" + strconv.Itoa(tempListing.ID)).Scan(&tempListing)
+		(*listingList) = append((*listingList), tempListing)
+	}
+	return retVal.Error
 }
 
-// GetPassword gets the hashed password for the user
-func (u *User) GetPassword(db *gorm.DB) {
-	row := db.Raw("SELECT password FROM users where email='" + u.Email + "'").Row()
-	var pass string
-	row.Scan(&pass)
-	u.Password = pass
+// AddInvitation adds a new invitation to the user
+func (u *User) AddInvitation(db *gorm.DB, invitationID int) error {
+	now := time.Now()
+	u.UpdatedAt = now
+	u.Invitations = append(u.Invitations, int64(invitationID))
+	retVal := db.Save(&u).Table("users")
+	return retVal.Error
+}
+
+// RemoveInvitation removes the specified invitation from the user
+func (u *User) RemoveInvitation(db *gorm.DB, invitationID int) error {
+	now := time.Now()
+	u.UpdatedAt = now
+	for i, g := range u.Invitations {
+		if g == int64(invitationID) {
+			u.Invitations = RemoveElement(u.Invitations, i)
+			break
+		}
+	}
+	retVal := db.Save(&u).Table("users")
+	return retVal.Error
+}
+
+// GetInvitations retrieves the invitation objects under the user
+func (u *User) GetInvitations(db *gorm.DB, invitationList *[]Invitation) error {
+	retVal := db.Raw("SELECT * FROM users WHERE ID=" + strconv.Itoa(u.ID)).Scan(&u)
+	for _, g := range u.Invitations {
+		tempInvitation := Invitation{ID: int(g)}
+		db.Raw("SELECT * FROM invitations WHERE ID=" + strconv.Itoa(tempInvitation.ID)).Scan(&tempInvitation)
+		(*invitationList) = append((*invitationList), tempInvitation)
+	}
+	return retVal.Error
 }
 
 // DBMigrate will create and migrate the tables, and then make the relationships
@@ -158,6 +226,6 @@ func DBMigrate(db *gorm.DB) *gorm.DB {
 	db.AutoMigrate(&Token{})
 	db.AutoMigrate(&Group{})
 	db.AutoMigrate(&Listing{})
-	db.AutoMigrate(&Request{})
+	db.AutoMigrate(&Invitation{})
 	return db
 }
