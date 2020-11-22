983d30206d6ce2f67b98036576b3fc73d110b2e8
shirlyfang <shirly.is.here@gmail.com>
2020-11-21 23:42:30 -0800

    initial tests

--- cs130_front/src/App.test.js
+++ cs130_front/src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { Route, Switch, MemoryRouter } from 'react-router-dom';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { render, fireEvent, waitFor, screen, getByTestId, within, queryByTestId } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import LandingPage from './components/views/LandingPage/LandingPage';
+import RightMenu from './components/views/NavBar/Sections/RightMenu';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  
+test('Landing Page join goes to Register', async () => {
+  const mockHistoryPush = { push: jest.fn() };
+  //set json object to storage 
+  render(
+    <MemoryRouter initialEntries={[`/`]}>
+      <LandingPage history={mockHistoryPush}/>
+    </MemoryRouter>
+  );
+  fireEvent.click(screen.getByRole ('button'));
+  expect(mockHistoryPush.push.mock.calls[0]).toEqual([ '/register'])
+    //remove object
+}); 
+
+test('Right Menu goes to Register', async () => {
+  const mockHistoryPush = { push: jest.fn() };
+  //set json object to storage 
+  render(
+    <MemoryRouter initialEntries={[`/`]}>
+      <RightMenu history={mockHistoryPush}/>
+    </MemoryRouter>
+  );
+  screen.debug()
+  fireEvent.click(screen.getByText ('Profile'));
+  console.log(mockHistoryPush.push.mock)
+  //expect(mockHistoryPush.push.mock.calls[0]).toEqual([ '/register'])
+    //remove object
+}); 
