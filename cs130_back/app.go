a84984e1ceb936e7f38ca4bdcb9c0647d2e26345
Varun Raju <36458233+Acabado@users.noreply.github.com>
2020-11-24 13:40:43 -0800

    Added methods to fetch all users (for search) and to fetch a user's course details (#70)
    * Add function to retrieve all users in DB
    
    * Get user's Studdy Buddies/ Listings for their courses

--- cs130_back/app.go
+++ cs130_back/app.go
@@ -75,7 +75,9 @@ func (a *App) initializeRoutes() {
 	authUserRoutes.HandleFunc("/removeCourse", a.handleRequest(handlers.RemoveCourse)).Methods("PUT")
 	authUserRoutes.HandleFunc("/updateListing", a.handleRequest(handlers.UpdateListing)).Methods("PUT")
 	authUserRoutes.HandleFunc("/updateInvitation", a.handleRequest(handlers.UpdateInvitation)).Methods("PUT")
+	authUserRoutes.HandleFunc("/getBuddiesListings", a.handleRequest(handlers.GetBuddiesAndListings)).Methods("GET")
 	authUserRoutes.HandleFunc("/deleteListing", a.handleRequest(handlers.DeleteListing)).Methods("DELETE")
+
 	//Group Routes
 	groupRoutes := routes.PathPrefix("/group").Subrouter()
 	groupRoutes.HandleFunc("/create", a.handleRequest(handlers.CreateGroup)).Methods("POST")
@@ -83,6 +85,10 @@ func (a *App) initializeRoutes() {
 	//Course Routes
 	courseRoutes := routes.PathPrefix("/course").Subrouter()
 	courseRoutes.HandleFunc("/addListing", a.handleRequest(handlers.CreateListing)).Methods("POST")
+
+	//Miscellaneous
+	routes.HandleFunc("/getAllUsers", a.handleRequest(handlers.GetAllUsers)).Methods("GET")
+	
 }
 
 func (a *App) loginMiddleware(next http.Handler) http.Handler {
