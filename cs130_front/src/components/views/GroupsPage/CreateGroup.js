77b5aee65abf48c137bd2187daedf8d11cce9f60
ahilleary <33604437+ahilleary@users.noreply.github.com>
2020-11-23 19:00:32 -0800

    Nearly done with visuals (#66)
    * Nearly done with visuals
    
    * Finished the commented changes
    
    * Added changes to formatting
    
    Co-authored-by: Alexander Trujillo-Hilleary <alexander@Alexanders-MacBook-Pro.local>

---
+++ cs130_front/src/components/views/GroupsPage/CreateGroup.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import FormInput from '../../Infos/FormInput';
+import Button from '../../Button/Button';
+import * as Colors from '../../../constants/Colors';
+import './styles.css';
+
+export default function CreateGroup(props) {
+    const [firstInput, setFirstInput] = useState({ name: props.options[0].name, value: props.options[0].value });
+    function sendInput(name, value) {
+        switch (name) {
+            case firstInput.name:
+                setFirstInput({ name: firstInput.name, value: value })
+                break;
+            default:
+                break;
+        }
+    }
+    return (
+        <div className="col">
+            {props.options.map(option => <FormInput key={option.name} option={option} sendInput={sendInput} />)}
+            <Button
+                textColor={Colors.White}
+                textSize="28px"
+                width="275px"
+                height="45px"
+                textWeight="800"
+                color={Colors.Blue}
+                onClick={() => props.saveInfoClicked(firstInput)}
+            >
+                Create Group
+            </Button>
+        </div>
+    );
+}
--- cs130_front/src/components/views/GroupsPage/GroupsPage.js
+++ cs130_front/src/components/views/GroupsPage/GroupsPage.js
@@ -1,35 +1,104 @@
 import React, { useState, useEffect } from 'react';
+import ClassList from '../../CurrentClasses/ClassList';
+import GroupItem from './GroupItem';
+import Text from '../../Text/Text';
+import Button from '../../Button/Button';
+import Requests from '../../Requests/Requests';
+import * as Colors from '../../../constants/Colors';
 import UserList from '../../UserList/UserList';
 import '../../../App.css';
+import CreateGroup from './CreateGroup';
 
 function GroupsPage(props) {
 
 
-  const classes = [
-    {name: "Discrete Mathematics", courseId: 1, groups: [{name: "DM Squad", groupId: 1}, {name: "DM Squad II", groupId: 1}] }, 
-    {name: "Computer Architecture", courseId: 2, groups: [{name: "CA Squad", groupId: 3}]}
+  const members = [
+    { name: "Shirly fang", school: "UCLA", id: 123, discord: "shirly#123", email: "shirly@gmail.com" },
+    { name: "Shirly fang", id: 123, discord: "shirly#123", email: "shirly@gmail.com" }
   ]
-  const [mainPanelState, setMainPanel] = useState();
 
-  const members= [
-    {name: "Shirly fang", school: "UCLA", id:123, discord:"shirly#123", email:"shirly@gmail.com"},
-    {name: "Shirly fang", id:123, discord:"shirly#123", email:"shirly@gmail.com"}
+  const currentGroup2 = { id: 123, name: "DM Squad", courseName: "Calculus", members: members, day: "friday", time: "4:30pm" }
+
+  const classes2 = [
+    { name: "Discrete Mathematics", courseId: 1, groups: [currentGroup2], },
+    { name: "Computer Architecture", courseId: 2, groups: [currentGroup2] }
   ]
+
+  const groupInformation = [
+    { name: "Group name", value: "" },
+  ];
+
+  const reqs = [{ name: "Al Squad", id: 223, types: "invitation" }, { name: "Calc Gang", id: 223, types: "invitation" }]
+
+  const [requests, setRequests] = useState(reqs);
+  const [currentGroup, setCurrentGroup] = useState(classes2[0].groups[0]);
+  const [classes, setClasses] = useState(classes2);
+
   // Pass this to ClassList
-  function groupClicked(title) {
+  function groupClicked(group) {
     //set main content to be for title
-    setMainPanel(title);
+    setCurrentGroup(group);
   }
 
+  function getClassesList() {
+    setClasses(classes2);
+  }
+
+  function getCourse(courseId) {
+    //get course from backend
+  }
+
+  function getMeetingTime() {
+    //get time from backend
+  }
+
+  function handleRequest(request) {
+    //update request with accept/decline
+    // and remove from list then fetch again to update ui
+  }
 
   const goToUserProfile = user => () => { props.history.push(`/profile/${user.id}`); }
 
+  function renderMainPanel() {
+    switch (currentGroup) {
+      case 'noGroupSelected':
+        return <Text color="black" size="24px" weight="800"> Select a group! </Text>
+      default:
+        return <GroupItem className="group-with-margin-centered" group={currentGroup} />
+    }
+  }
+  //{getCourse(currentGroup.courseId).name}
+  function myGroupAdmin() {
+    return (
+      <div className="panel">
+        <div className="column">
+          <div className="text-container">
+            <Text color="black" size="24px" weight="800">
+              {currentGroup.courseName}
+            </Text>
+          </div>
+          <CreateGroup options={groupInformation} />
+          <Requests title="Requests" items={requests} handleResponse={handleRequest} />
+        </div>
+        <div className="column">
+          {renderMainPanel()}
+        </div>
+        <div className="column">
+          <div className="group-with-margin-bottom">
+            <ClassList classList={classes} titleClicked={groupClicked} clickable={true} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-  return (
+  /*return (
     <div className="App">
       <UserList users={members} goToUserProfile={goToUserProfile} optionalElement={true} optionalClick={() => {}}/>
     </div>
-  );
+  );*/
+  return myGroupAdmin();
+
 }
 
 export default GroupsPage;
