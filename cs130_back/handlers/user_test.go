6e4522bf3f4db276ee2514478aa0e6c7228435e8
= <heysamlee@gmail.com>
2020-11-27 22:56:23 -0800

    added classes info test

--- cs130_back/handlers/user_test.go
+++ cs130_back/handlers/user_test.go
@@ -12,9 +12,38 @@ import (
 	"github.com/gavv/httpexpect"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 )
 
 var columns = []string{"id", "first_name", "last_name", "email"}
+
+var classColumns = []string{"id", "CreatedAt", "UpdatedAt", "name", "keywords", "categories", "study_buddies"}
+var categories = []string{"Math", "Biology"}
+
+func TestClassesInfo(t *testing.T) {
+	mock, DB := GetMock();
+	defer DB.Close()
+
+	handler := GetHandler(DB)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM courses`)).
+		WillReturnRows(sqlmock.NewRows(classColumns).AddRow(1, c, c, "Anatomy", nil, pq.Array(categories), nil))
+
+	obj :=	e.GET("/classes-info").
+			Expect().
+			Status(http.StatusOK).JSON().Object()
+
+	obj.Keys().Contains("courses")
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
 
 func TestGetAllUsersSuccess(t *testing.T) {
 	mock, DB := GetMock();
@@ -78,6 +107,26 @@ func GetMock() (sqlmock.Sqlmock, *gorm.DB) {
 func GetHandler(db *gorm.DB) http.Handler {
 	
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		CreateUser(db, w, r)
+	})
+
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		LoginUser(db, w, r)
+	})
+
+	mux.HandleFunc("/classes-info", func(w http.ResponseWriter, r *http.Request) {
+		GetClassesInfo(db, w, r)
+	})
+
+	mux.HandleFunc("/updateInvitation", func(w http.ResponseWriter, r *http.Request) {
+		UpdateInvitation(db, w, r)
+	})
+
+	mux.HandleFunc("/addCourse", func(w http.ResponseWriter, r *http.Request) {
+		AddCourse(db, w, r)
+	})
 
 	mux.HandleFunc("/getBuddiesListings", func(w http.ResponseWriter, r *http.Request) {
 		GetBuddiesAndListings(db, w, r)
